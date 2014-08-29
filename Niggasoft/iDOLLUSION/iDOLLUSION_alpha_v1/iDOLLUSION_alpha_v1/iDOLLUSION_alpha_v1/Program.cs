749990d218774bfb851307729ed66dd7063610ff
xnenaixyinaa <rxjohnston@gmail.com>
2014-08-29 00:49:13 -0400

    i hate github at this particular moment

---
+++ Niggasoft/iDOLLUSION/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace iDOLLUSION_alpha_v1
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+                
+            }
+        }
+    }
+#endif
+}
+
