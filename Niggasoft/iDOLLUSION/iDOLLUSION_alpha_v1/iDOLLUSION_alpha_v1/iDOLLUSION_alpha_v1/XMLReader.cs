68ccc64de4ea7c4c8ea8f81566929ce28184138d
Eternityis <ccarmo@optonline.net>
2014-08-29 15:50:00 -0400

    fdksal

---
+++ Niggasoft/iDOLLUSION/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/XMLReader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using System.Xml;
+
+
+public class XMLReader
+{
+    public static void readText()
+    {
+        string path = @"C:\Users\CJ\Desktop\test.xml";
+        XmlDocument dialogue = new XmlDocument();
+        dialogue.Load(path);
+        XmlNodeList nodelist = dialogue.SelectNodes("/Greeting"); // get all <testcase> nodes
+
+        foreach (XmlNode node in nodelist) // for each <testcase> node
+        {
+            try
+            {
+                string name = node.SelectSingleNode("ToProducer").InnerText;
+                 Console.WriteLine(name);
+
+            }
+            catch (Exception e)
+            {
+                
+            }
+
+        }
+
+    }
+}
