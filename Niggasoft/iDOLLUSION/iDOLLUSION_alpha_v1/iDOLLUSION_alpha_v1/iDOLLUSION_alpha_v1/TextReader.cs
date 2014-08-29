65f99ceaf6a4251d31b3accc941e383854c72f06
drewzilla5 <andrewnsalerno@gmail.com>
2014-08-29 01:02:02 -0400

    Andrew's a fucking hero you're welcome

---
+++ Niggasoft/iDOLLUSION/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/TextReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.IO;
+public class TextReader
+{
+	public void readText()
+	{
+        string path = @"C:\Users\Andrew\Downloads\idollusion\RoKu-PRO\Niggasoft\iDOLLUSION\iDOLLUSION_alpha_v1\iDOLLUSION_alpha_v1\Data\thenwhowasphone.txt"; //..\..\Data\thenwhowasphone.txt
+        using (StreamReader sr = File.OpenText(path))
+        {
+            string s = "";
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+	}
+}
