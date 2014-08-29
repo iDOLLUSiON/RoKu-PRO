7c7a5ea32cc038811d83503522ab4793f08e333b
xnenaixyinaa <rxjohnston@gmail.com>
2014-08-28 22:27:28 -0400

    sync

--- Niggasoft/iDOLLUSION/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/Game1.cs
+++ Niggasoft/iDOLLUSION/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/Game1.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -16,13 +17,16 @@ namespace iDOLLUSION_alpha_v1
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        private Texture2D background, silverButton, goldButton;
+        private Texture2D background, splash, silverButton, goldButton;
         private SpriteFont gameFont;
         private Rectangle backgroundRect, silverButtonRect, goldButtonRect;
         private int screenWidth, screenHeight;
+        bool atSplash = true;            
+
 
         public Game1()
         {
+
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
@@ -45,20 +49,34 @@ namespace iDOLLUSION_alpha_v1
             silverButton = Content.Load <Texture2D> ("sprites/silverButton");
             goldButton = Content.Load<Texture2D>("sprites/goldButton");
             background = Content.Load<Texture2D>("images/background");
+            splash = Content.Load<Texture2D>("images/splash");
         }
         protected override void UnloadContent()
         {
         }
         protected override void Update(GameTime gameTime)
         {
+            
             if (Keyboard.GetState().IsKeyDown(Keys.Space)) Exit();
+            if (atSplash && (Mouse.GetState().LeftButton == ButtonState.Pressed))
+            {
+                atSplash = false;
+            }
+            
             base.Update(gameTime);
         }
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
-            spriteBatch.Draw(background,backgroundRect,Color.White);
+            if (atSplash)
+            {
+                spriteBatch.Draw(splash, backgroundRect, Color.White);
+            }
+            else
+            {
+                spriteBatch.Draw(background, backgroundRect, Color.White);
+            }
             spriteBatch.Draw(silverButton, silverButtonRect,Color.White );
             spriteBatch.Draw(goldButton, goldButtonRect, Color.White); 
             spriteBatch.End();
--- Niggasoft/iDOLLUSION/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/Program.cs
+++ Niggasoft/iDOLLUSION/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace iDOLLUSION_alpha_v1
 {
@@ -13,6 +14,7 @@ namespace iDOLLUSION_alpha_v1
             using (Game1 game = new Game1())
             {
                 game.Run();
+                
             }
         }
     }
