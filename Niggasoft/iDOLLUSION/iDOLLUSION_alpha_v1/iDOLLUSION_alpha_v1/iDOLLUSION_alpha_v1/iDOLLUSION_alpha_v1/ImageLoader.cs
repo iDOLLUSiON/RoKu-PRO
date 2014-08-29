ae31e22677b56bbefc1caacf1d4b3118576cd482
xnenaixyinaa <rxjohnston@gmail.com>
2014-08-29 00:09:12 -0400

    update?

--- Niggasoft/iDOLLUSION/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/ImageLoader.cs
+++ Niggasoft/iDOLLUSION/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/ImageLoader.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace iDOLLUSION_alpha_v1
 {
     class ImageLoader
     {
+        private Texture2D background, splash, silverButton, goldButton, silverButtonR, goldButtonR, mouseIcon;
     }
 }
