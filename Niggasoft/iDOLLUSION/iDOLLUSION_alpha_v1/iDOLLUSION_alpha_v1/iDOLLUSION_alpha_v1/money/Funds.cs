336b16d5b8e56b99acf8625099fdbb838ebb311d
drewzilla5 <andrewnsalerno@gmail.com>
2014-08-29 22:51:57 -0400

    Cash and Funds merged into Funds
    fundsToString method created

--- Niggasoft/iDOLLUSION/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/money/Funds.cs
+++ Niggasoft/iDOLLUSION/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/money/Funds.cs
@@ -5,21 +5,68 @@ using System.Text;
 
 namespace iDOLLUSION_alpha_v1
 	{
-   static class Funds // the money the company has, not your personal wages
-		{
-	private static long funds = 0; //starting funds
+    static class Funds // the money the company has, not your personal wages
+	{
+	        private static int peronsalFunds = 0; //starting funds
+            private static int companyFunds = 0;
+	        public static void setFunds(int val, string accType) 
+            {
+                switch (accType)
+                {
+                    case "personal":
+                        peronsalFunds = val;
+                        break;
+
+                    case "company":
+                        companyFunds = val;
+                        break;
+                }
+	        }
 	
-	public static void setFunds(long val) {
-		funds = val;
-	}
-	
-	public static long getFunds() {
-		return funds;
-	}
-	
-	public static void addFunds( int addFunds ) {
-		funds += addFunds;
-	}
+	        public static int getFunds(string accType) 
+            {
+                switch (accType)
+                {
+                    case "personal":
+                        return peronsalFunds;
+
+                    case "company":
+                        return companyFunds;
+                }
+                return 1000000000;
+	        }
+
+            public static void addFunds(int val, string accType) 
+            {
+                switch (accType)
+                {
+                    case "personal":
+                        peronsalFunds += val;
+                        break;
+
+                    case "company":
+                        companyFunds += val;
+                        break;
+                }
+	        }
+        }
+            public static string fundsToString(string accType)
+            {
+                switch(accType)
+                    case "personal":
+                        string funds = personalFunds.ToString();
+                        break;
+                    case "company":
+                        string funds = companyFunds.ToString()
+                        break;
+
+                int length = funds.Length; 
+
+                for(int i = 1, length/3>i, i++)
+                {
+                    funds.Insert(i*3+(i-1),",");
+                }
+                return funds;  
 
 		}
 	}
