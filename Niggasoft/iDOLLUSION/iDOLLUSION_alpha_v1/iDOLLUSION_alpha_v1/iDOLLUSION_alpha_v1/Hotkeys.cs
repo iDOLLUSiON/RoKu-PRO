8710faab3d7bf9ef40ba9a222a0300faa0b0a8d7
drewzilla5 <andrewnsalerno@gmail.com>
2014-08-31 01:00:09 -0400

    hotkeys working off the system.windows.forms lib

---
+++ Niggasoft/iDOLLUSION/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/Hotkeys.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace iDOLLUSION_alpha_v1
+{
+    class Hotkeys
+    {
+        private void HotKey(object sender, KeyEventArgs e)
+        {
+            // Determine if the key entered matches a hotkey
+            if (e.KeyCode == Keys.P)
+            {
+                //Make a thing happen
+                
+            }
+            else if (e.KeyCode == Keys.M)
+            {
+                //Make a thing happen
+
+            }
+        }
+    }
+}
