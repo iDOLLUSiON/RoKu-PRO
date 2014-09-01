b84964224e427f5be537929b9986bfa7c2d1f4e8
Eternityis <ccarmo@optonline.net>
2014-08-31 21:41:08 -0400

    added more to units. Error handling

--- Niggasoft/iDOLLUSION/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/characters/Units.cs
+++ Niggasoft/iDOLLUSION/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/iDOLLUSION_alpha_v1/characters/Units.cs
@@ -15,6 +15,7 @@ namespace iDOLLUSION_alpha_v1.characters
             private List<Idol> unitMembers = new List<Idol>(); //list of idols in each unit
             //static lists
             private static List<Units> unitList = new List<Units>(); //list of all units
+            private static List<Idol> idolsInUnits = new List<Idol>(); //list of all idol in units, to prevent "double booking" problems down the line
             private static List<Idol> unitLeaders = new List<Idol>(); //List of all unit leaders
 //Unit variables
             private string unitName; //the name of hte unit as a whole eg AKB48
@@ -25,6 +26,12 @@ namespace iDOLLUSION_alpha_v1.characters
 
             public Units(Idol idol1, Idol idol2)
             {
+            //check for problems
+                if (idolsInUnits.Contains(idol1) || idolsInUnits.Contains(idol2))
+                {
+                    return;  //if the idols are already in units, then no new unit can be made with them
+                }
+        
                 string newUnitName = Microsoft.VisualBasic.Interaction.InputBox("Input Unit Name",
                     "Create a New Duo Unit:", "", 640, 360); //weird resharper formatting, sorry.   But basically this just makes a popup textbox. Imported from VB
                     //This all works!  The box itself needs to be prettied up tho!
@@ -34,14 +41,23 @@ namespace iDOLLUSION_alpha_v1.characters
                 this.unitMembers.Add(idol1);
                 this.unitMembers.Add(idol2);
                 this.unitLeader = idol1; //set idol1 as unitleader of this unit
+                this.unitUnity = generateUnityValue(unitMembers);
+                //static
                 unitLeaders.Add(idol1); //add "idol1" to the list of unitleaders
-                unitUnity = generateUnityValue(unitMembers);
+                idolsInUnits.Add(idol1);
+                idolsInUnits.Add(idol2);
+
+                
 
 
             }
 
             public Units(Idol idol1, Idol idol2, Idol idol3)
             {
+                if (idolsInUnits.Contains(idol1) || idolsInUnits.Contains(idol2) || idolsInUnits.Contains(idol3))
+                {
+                    return;
+                }
                 string newUnitName = Microsoft.VisualBasic.Interaction.InputBox("Input Unit Name",
                     "Create a New Trio Unit:", "", 640, 360);
                     //This all works!  The box itself needs to be prettied up tho!
@@ -51,14 +67,22 @@ namespace iDOLLUSION_alpha_v1.characters
                 this.unitMembers.Add(idol2);
                 this.unitMembers.Add(idol3);
                 this.unitLeader = idol1; //set idol1 as unitleader of this unit
-                unitLeaders.Add(idol1);
-                unitUnity = generateUnityValue(unitMembers);
+                this.unitUnity = generateUnityValue(unitMembers);
+             //static
+               unitLeaders.Add(idol1);
+                idolsInUnits.Add(idol1);
+                idolsInUnits.Add(idol2);
+                idolsInUnits.Add(idol3);
 
 
             }
 
             public Units(Idol idol1, Idol idol2, Idol idol3, Idol idol4)
             {
+                if (idolsInUnits.Contains(idol1) || idolsInUnits.Contains(idol2) || idolsInUnits.Contains(idol3) || idolsInUnits.Contains(idol4))
+                {
+                    return;
+                }
                 string newUnitName = Microsoft.VisualBasic.Interaction.InputBox("Input Unit Name",
                     "Create a New 4-Member Unit:", "", 640, 360);
                     //This all works!  The box itself needs to be prettied up tho!
@@ -69,10 +93,13 @@ namespace iDOLLUSION_alpha_v1.characters
                 this.unitMembers.Add(idol3);
                 this.unitMembers.Add(idol4);
                 this.unitLeader = idol1; //set idol1 as unitleader of this unit
+                this.unitUnity = generateUnityValue(unitMembers); //Generates unity value by taking the unity value of all members of the list, averaging them, and then casting to int.
+                //static
                 unitLeaders.Add(idol1);
-                unitUnity = generateUnityValue(unitMembers); //Generates unity value by taking the unity value of all members of the list, averaging them, and then casting to int.
-
-
+                idolsInUnits.Add(idol1);
+                idolsInUnits.Add(idol2);
+                idolsInUnits.Add(idol3);
+                idolsInUnits.Add(idol4);
             }
 
 
